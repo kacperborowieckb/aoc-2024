a7aeb5c30356c09d54aab8ec45022b877d2c6011
kacperborowieckb <kacperborowiec.kb@gmail.com>
2024-12-05 17:50:53 +0100

    feat: day 2 solution

---
+++ day2/part1.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+
+const content = fs.readFileSync('input.txt').toString();
+
+const games = content.split('\n').map((row) => row.split(' '));
+
+function checkDecreasing(firstVal, secondVal) {
+  return firstVal - secondVal > 0;
+}
+
+function checkStep(firstVal, secondVal) {
+  const step = Math.abs(firstVal - secondVal);
+
+  return step >= 1 && step <= 3;
+}
+
+function isSafe(game) {
+  const decreasing = checkDecreasing(game[0], game[1]);
+
+  return game.every((record, i) => {
+    const [first, second] = [Number(record), Number(game[i + 1])];
+
+    if (!second) return true;
+
+    if (decreasing !== checkDecreasing(first, second)) return false;
+
+    if (!checkStep(first, second)) return false;
+
+    return true;
+  });
+}
+
+function solve() {
+  return games.reduce((validGames, game) => {
+    const isValidGame = isSafe(game);
+
+    return isValidGame ? validGames + 1 : validGames;
+  }, 0);
+}
+console.log(solve());
